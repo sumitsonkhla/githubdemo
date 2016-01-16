304ddeb23e583e1b275b75a67f7f386d8eb8ee00
Judas <sumitsonkhla@gmail.com>
2016-01-16 22:06:07 +0530

    additional changes

--- GITHubDemo/src/com/github/Basic.java
+++ GITHubDemo/src/com/github/Basic.java
@@ -5,5 +5,6 @@ public class Basic {
 	public static void main(String args[]){
 		System.out.println("Welocmeto the Git");
 		System.out.println("making first change");
+		System.out.println("pulling out and checking in");
 	}
 }
