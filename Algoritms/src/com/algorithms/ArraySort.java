394e2d2553d92fa66843935a20e915da143888b6
Judas <sumitsonkhla@gmail.com>
2016-08-27 14:52:21 +0530

    Pushing Algorithms

---
+++ Algoritms/src/com/algorithms/ArraySort.java
@@ -0,0 +1,17 @@
+package com.algorithms;
+
+public class ArraySort {
+
+public static void main(String[] args) {
+	
+	int arr[]={1,2,3,10,5,6};
+	System.out.println(checkSort(arr,5));
+}
+
+static boolean checkSort(int [] arr, int index){
+	if(index<1){
+		return true;
+	}
+	return arr[index]<arr[index-1]? false:checkSort(arr,index-1);
+}
+}
