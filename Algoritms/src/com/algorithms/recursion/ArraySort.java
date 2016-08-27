a7922ddd4f379b8029fc05ceeefd63da68897a43
Judas <sumitsonkhla@gmail.com>
2016-08-28 00:30:03 +0530

    Adding few more changes.

--- Algoritms/src/com/algorithms/recursion/ArraySort.java
+++ Algoritms/src/com/algorithms/recursion/ArraySort.java
@@ -1,4 +1,4 @@
-package com.algorithms;
+package com.algorithms.recursion;
 
 public class ArraySort {
 
--- Algoritms/src/com/algorithms/recursion/SequenceGenerator.java
+++ Algoritms/src/com/algorithms/recursion/SequenceGenerator.java
@@ -1,4 +1,4 @@
-package com.algorithms;
+package com.algorithms.recursion;
 
 import java.util.Scanner;
 
